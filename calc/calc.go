1b4beafc004e6c80d4181793e308e68cf63264c7
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-06-10 21:54:24 -0600

    Fix golint issues

--- calc/calc.go
+++ calc/calc.go
@@ -1,24 +1,24 @@
 package calc
 
-// Adds two numbers.
+// Add two numbers.
 // Returns the result.
 func Add(a, b int) int {
 	return a+b
 }
 
-// Subtracts two numbers.
+// Subtract two numbers.
 // Returns the result.
 func Subtract(a, b int) int {
 	return a-b
 }
 
-// Multiples two numbers.
+// Multiple two numbers.
 // Returns the result.
 func Multiply(a, b int) int {
 	return a*b
 }
 
-// Divides two numbers.
+// Divide two numbers.
 // Returns the result.
 func Divide(a, b int) float64 {
 	return float64(a/b)
