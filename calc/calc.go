caa4a9a5c40353a04560ba0c9fb6fac7eb16dee1
Benjamin Muschko <benjamin.muschko@gmail.com>
2018-06-10 22:07:54 -0600

    Fix formatting

--- calc/calc.go
+++ calc/calc.go
@@ -1,25 +1,25 @@
 package calc
 
 // Add two numbers.
-// Returns the result.
+// Return the result.
 func Add(a, b int) int {
 	return a + b
 }
 
 // Subtract two numbers.
-// Returns the result.
+// Return the result.
 func Subtract(a, b int) int {
 	return a - b
 }
 
 // Multiply two numbers.
-// Returns the result.
+// Return the result.
 func Multiply(a, b int) int {
 	return a * b
 }
 
 // Divide two numbers.
-// Returns the result.
+// Return the result.
 func Divide(a, b int) float64 {
 	return float64(a / b)
 }
